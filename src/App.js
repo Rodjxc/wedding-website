182b049d4339d28ff231a66b1fadb38673d78cef
Rodjxc <118216997+Rodjxc@users.noreply.github.com>
2023-03-17 17:16:42 +0100

    Add files via upload
    Uploaded initial React files for the projects

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import styles from "../public/estilos.css"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Wedding website</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App;
